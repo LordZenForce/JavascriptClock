a084cfc271e522f1fe222ffd7ea2d0157bc92499
Jean <Jamin4me@gmail.com>
2021-02-16 17:44:43 -0500

    Update clock.js

--- clock.js
+++ clock.js
@@ -13,7 +13,8 @@ CurrentHours = (CurrentHours > 12) ? CurrentHours - 12 : CurrentHours;
 CurrentHours = (CurrentHours == 0) ? 12 : CurrentHours;
 
 var CurrentTimeString = CurrentHours + ":" + CurrentMinutes + ":" + CurrentSeconds + " " + TimeOfDay;
-        
+ 
+document.getElementById("clock.js").firstChild.nodeValue = currentTimeString;
 }
     
-  document.getElementById("clock.js").firstChild.nodeValue = currentTimeString;
+  
