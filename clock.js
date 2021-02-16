de54e4ca491ec50337ccaee8584839f0191af28c
Jean <Jamin4me@gmail.com>
2021-02-16 17:39:20 -0500

    Update clock.js

--- clock.js
+++ clock.js
@@ -1,4 +1,6 @@
-var CurrentTime = new Date ();
+function updateClock ()
+    {
+    var CurrentTime = new Date ();
 var CurrentHours = currentTime.getHours ();
 var CurrentMinutes = currentTime.getMinutes ();
 var CurrentSeconds = currentTime.getSeconds ();
@@ -6,13 +8,13 @@ var CurrentSeconds = currentTime.getSeconds ();
 CurrentMinutes = (CurrentMinutes < 10 ? "0" : " ") + CurrentMinutes;
 CurrentSeconds = (CurrentSeconds < 10 ? "0" : " ") + CurrentSeconds;
 
-var TimeOfDay = (CurrentHours < 12) ? "AM" : "PM
+var TimeOfDay = (CurrentHours < 12) ? "AM" : "PM";
 CurrentHours = (CurrentHours > 12) ? CurrentHours - 12 : CurrentHours;
 CurrentHours = (CurrentHours == 0) ? 12 : CurrentHours;
 
 var CurrentTimeString = CurrentHours + ":" + CurrentMinutes + ":" + CurrentSeconds + " " + TimeOfDay;
 
-<span id="clock"> &nbsp; </span>
-<value>document.GetElementById("jsclock").firstChild.nodeValue= currentTimeString;
-      
-
+document.getElementById("clock").firstChild.nodeValue = currentTimeString;
+}
+    
+  
