4a78507e379e4b54501f8cfffe24a35b21d0522c
Jean <Jamin4me@gmail.com>
2021-02-16 16:54:00 -0500

    Create clock.js

---
+++ clock.js
@@ -0,0 +1,13 @@
+var CurrentTime = new Date ();
+var CurrentHours = currentTime.getHours ();
+var CurrentMinutes = currentTime.getMinutes ();
+var CurrentSeconds = currentTime.getSeconds ();
+
+CurrentMinutes = (CurrentMinutes < 10 ? "0" : " ") + CurrentMinutes;
+CurrentSeconds = (CurrentSeconds < 10 ? "0" : " ") + CurrentSeconds;
+
+var TimeOfDay = (CurrentHours < 12) ? "AM" : "PM
+CurrentHours = (CurrentHours > 12) ? CurrentHours - 12 : CurrentHours;
+CurrentHours = (CurrentHours == 0) ? 12 : CurrentHours;
+
+var CurrentTimeString = CurrentHours + ":" + CurrentMinutes + ":" + CurrentSeconds + " " + TimeOfDay;
